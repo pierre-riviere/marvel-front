3b6b98f8fd7a886314744b284fd697f833d3e7c1
Pierre <pierreriviere@hotmail.com>
2020-08-02 11:59:42 +0200

    🔧 Update prod config file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  marvelBackApiUrl: 'http://localhost:3000/api',
+  marvelBackApiUrl: 'http://ec2-35-180-89-101.eu-west-3.compute.amazonaws.com:3000/api',
 };
